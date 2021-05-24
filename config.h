f8f7454f65541c834a2d4586cca26ccce68c8a41
Leo Liu <auleo.liu@gmail.com>
2021-05-24 11:03:07 +1000

    switchtotag

--- config.h
+++ config.h
@@ -36,8 +36,8 @@ static const Rule rules[] = {
      *	WM_CLASS(STRING) = instance, class
      *	WM_NAME(STRING) = title
      */
-    /* class     instance  title           tags mask  isfloating  isterminal
-       noswallow  monitor */
+    /* class  instance  title tags mask isfloating  isterminal noswallow
+       monitor */
     {"Gimp", NULL, NULL, 0, 1, 0, 0, -1},
     {"Firefox", NULL, NULL, 1 << 8, 0, 0, -1, -1},
     {"St", NULL, NULL, 0, 0, 1, 0, -1},
